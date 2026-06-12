3fad7044fd46a2610aa9ca5251dc81bf1a58f9f1
Abhiroop <asiamgenius@gmail.com>
2026-06-12 02:31:17 +0200

    More Chap4

--- PractiseLean/Chap4.lean
+++ PractiseLean/Chap4.lean
@@ -84,8 +84,92 @@ theorem Forall.one_point' {α : Type} (t : α) (P : α → Prop) :
     (fun hall => hall t rfl)
     (fun hp => fun _ => fun heq => heq ▸ hp)
 
+
+#check And.intro
+#check Exists.intro
+
 theorem Exists.one_point {α : Type} (t : α) (P : α → Prop) :
     (∃x : α, x = t ∧ P x) ↔ P t :=
-  sorry
+  Iff.intro
+    (fun hex => Exists.elim hex (fun x hxp =>
+                                  let heq := And.left  hxp
+                                  let hpx := And.right hxp
+                                  heq ▸ hpx))
+    (fun pt => Exists.intro t (And.intro rfl pt))
+
+theorem Exists.one_point' {α : Type} (t : α) (P : α → Prop) :
+    (∃x : α, x = t ∧ P x) ↔ P t :=
+  Iff.intro
+    (assume hex : ∃ x, x = t ∧ P x
+     show P t from
+      Exists.elim hex
+        (fix x : α
+         assume hand : x = t ∧ P x
+         have hxt : x = t :=
+          And.left hand
+         have hpx : P x :=
+          And.right hand
+         show P t from
+          by
+            rw [←hxt]
+            exact hpx)
+    )
+    (assume pt : P t
+     show ∃ x, x = t ∧ P x from
+     Exists.intro t
+      (have tt : t = t :=
+        by rfl
+        show t = t ∧ P t from
+          And.intro tt pt))
+
+#check Nat.two_mul
+
+theorem two_mul_example (m n : ℕ) :
+    2 * m + n = m + n + m :=
+  calc
+    2 * m + n = m + m + n :=
+      by rw [Nat.two_mul]
+    _ = m + n + m :=
+      by ac_rfl
+
+def reverse {α : Type} : List α → List α
+  | [] => []
+  | x :: xs => reverse xs ++ [x]
+
+#check Eq
+#check List.append_nil
+
+theorem reverse_append {α : Type} :
+  ∀ xs ys : List α,
+    reverse (xs ++ ys) = reverse ys ++ reverse xs := by
+  intro xs
+  induction xs with
+  | nil => intro ys
+           simp
+           rfl
+  | cons x xs ih => intro ys
+                    simp [reverse]
+                    rw [ih, List.append_assoc]
+
+theorem reverse_reverse {α : Type} :
+  ∀ xs : List α,
+    reverse (reverse xs) = xs := by
+  intro xs
+  induction xs with
+  | nil => simp [reverse]
+  | cons x xs ih => simp [reverse]
+                    rw [reverse_append, ih]
+                    simp [reverse]
+
+theorem reverse_reverse' {α : Type} :
+  ∀ xs : List α,
+    reverse (reverse xs) = xs
+  | [] => by rfl
+  | x :: xs => by
+                  simp [reverse]
+                  rw [reverse_append, reverse_reverse']
+                  simp [reverse]
+
+
 
 end Chap4
