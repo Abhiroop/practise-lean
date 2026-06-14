b2e42a52546b339525bcbaf4089451be013a6e44
Abhiroop <asiamgenius@gmail.com>
2026-06-14 04:18:51 +0200

    Stuff

--- PractiseLean/Chap5.lean
+++ PractiseLean/Chap5.lean
@@ -48,4 +48,39 @@ theorem map_ident {α : Type} (xs : List α) :
   | nil => rfl
   | cons x xs ih => rw [map, ih]
 
+theorem map_comp {α β γ : Type} (f : α → β) (g : β → γ)
+    (xs : List α) :
+  map g (map f xs) = map (fun x ↦ g (f x)) xs := by
+  induction xs with
+  | nil => rfl
+  | cons x xs ih => simp [map, ih]
+
+def headPre {α : Type} : (xs : List α) → xs ≠ [] → α
+  | [], hxs => by simp at *
+  | x :: _, hxs => x
+
+def zip {α β : Type} : List α → List β → List (α × β)
+  | x :: xs, y :: ys => (x, y) :: zip xs ys
+  | [], _ => []
+  | _ :: _, [] => []
+
+def length {α : Type} : List α → ℕ
+  | [] => 0
+  | x :: xs => length xs + 1
+
+theorem length_zip {α β : Type} (xs : List α) (ys : List β) :
+    length (zip xs ys) = min (length xs) (length ys) :=
+  by
+    induction xs generalizing ys with
+    | nil => simp [zip, length]
+    | cons x xs ih =>
+                      cases ys with
+                      | nil => simp [zip, length]
+                      | cons y ys' => simp [zip, length, ih]; omega
+
+
+inductive Vec (α : Type) : ℕ  → Type where
+  | nil : Vec α 0
+  | cons (a : α) {n : ℕ} (v : Vec α n) : Vec α (n + 1)
+
 end Chap5
