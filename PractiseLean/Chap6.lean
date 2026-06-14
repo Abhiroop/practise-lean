4ee89250df88f7f2ffe947f042c74a8ad78695cd
Abhiroop <asiamgenius@gmail.com>
2026-06-15 01:34:52 +0200

    Proof of stuff

--- PractiseLean/Chap6.lean
+++ PractiseLean/Chap6.lean
@@ -11,5 +11,33 @@ inductive Even : ℕ → Prop where
 theorem Even_4 :
   Even 4 := .add_two _ (.add_two _ .zero)
 
+#check Or.inl
+#check Exists.intro
+
+theorem Even_Iff_struct (n : ℕ) :
+    Even n ↔ n = 0 ∨ (∃m : ℕ, n = m + 2 ∧ Even m) :=
+  Iff.intro
+    ( assume hn : Even n
+      match hn with
+      | Even.zero =>
+        show 0 = 0 ∨ _ from
+          Or.inl rfl
+      | Even.add_two k hk =>
+        show _ ∨ ∃ m, k + 2 = m + 2 ∧ Even m from
+          Or.inr (Exists.intro k (And.intro rfl hk))
+    )
+    (assume hr : (n = 0 ∨ ∃ m, n = m + 2 ∧ Even m)
+     match hr with
+     | Or.inl heq => heq ▸ Even.zero
+     | Or.inr hex =>
+        match hex with
+        | ⟨ m, hand ⟩ =>
+          match hand with
+          | ⟨heq, heven ⟩ =>
+            show Even n from
+              have mplustwo : Even (m + 2) :=
+                Even.add_two m heven
+              heq ▸ mplustwo
+    )
 
 end Chap6
