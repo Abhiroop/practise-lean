b2daf6b2fc12c3ed15af35981f516e875794dcd7
Abhiroop <asiamgenius@gmail.com>
2026-06-15 15:26:07 +0200

    Completion

--- PractiseLean/Chap6.lean
+++ PractiseLean/Chap6.lean
@@ -40,4 +40,36 @@ theorem Even_Iff_struct (n : ℕ) :
               heq ▸ mplustwo
     )
 
+inductive Sorted : List ℕ → Prop where
+| nil : Sorted []
+| single (x : ℕ) : Sorted [x]
+| two_or_more (x y : ℕ) {zs : List ℕ} (hle : x ≤ y)
+      (hsorted : Sorted (y :: zs)) :
+  Sorted (x :: y :: zs)
+
+theorem Sorted_3_5 :
+    Sorted [3, 5] :=
+  Sorted.two_or_more _ _ (by simp) (Sorted.single _)
+
+theorem Not_Sorted_17_13 :
+    ¬ Sorted [17, 13] :=
+  by
+    intro h
+    cases h with
+    | two_or_more _ _ hlet hsorted => omega
+
+
+#check absurd
+
+theorem Not_Even_two_mul_add_one (m n : ℕ)
+    (hm : m = 2 * n + 1) :
+  ¬ Even m := by
+  intro hn
+  induction hn generalizing n with
+  | zero => omega
+  | add_two k a ih => apply ih (n - 1)
+                      omega
+
+
+
 end Chap6
